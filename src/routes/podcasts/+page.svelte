0279f3655f3779360f4a5a801ccb8194f0501104
Mario <magildo@alumni.uv.es>
2024-03-22 20:29:21 +0100

    Cambio en como se muestra la fecha

--- src/routes/podcasts/+page.svelte
+++ src/routes/podcasts/+page.svelte
@@ -4,12 +4,8 @@
   import "../../css/Noticias.css";
 
   function formatDate(date) {
-    const d = new Date(date);
-    const day = String(d.getDate()).padStart(2, '0');
-    const month = String(d.getMonth() + 1).padStart(2, '0'); // January is 0!
-    const year = d.getFullYear();
-
-    return `${day}-${month}-${year}`;
+    const formatter = new Intl.DateTimeFormat('es', { dateStyle: 'long' })
+    return formatter.format(new Date(date))
   }
 </script>
 
