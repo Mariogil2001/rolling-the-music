0b68a2b62f76050fae7d2634e366033e0ea5953a
Mario <magildo@alumni.uv.es>
2024-03-22 19:44:13 +0100

    Añadido Entrevista a Ruido Amor

--- src/js/podcasts.js
+++ src/js/podcasts.js
@@ -1,28 +1,38 @@
+const color = "2C3639";
+
 export const podcasts = [
   {
-    title: "Entrevista a May de \"Badlands\"",
-    url: "https://www.ivoox.com/player_ej_125548538_6_1.html?c1=2C3639",
+    title: "Entrevista a Ruido Amor",
+    url: "https://www.ivoox.com/player_ej_126327821_6_1.html?c1=" + color,
+    description:"Hoy entrevistamos a Noa, Alejandro y María de Ruido Amor, una banda emergente con estilo indie pop. Nos cuentan su experiencia y relación con la música y el lanzamiento de su nuevo EP ciclogénesis explosiva. Escúchanos y descubre cómo se formó la banda, cómo se conocieron y sus futuros proyectos!! Podrás escucharles en Spotify o el 4 de abril en el pub 46zero20 en vivo!!" ,
+    date: "2024-03-22",
+  },
+  {
+    title: 'Entrevista a May de "Badlands"',
+    url: "https://www.ivoox.com/player_ej_125548538_6_1.html?c1=" + color,
     description:
       "Hola! Bienvenidos a Rolling The music, un podcast donde hablamos de música y damos voz a músicos emergentes. Somos Maya y Julia y en este nuevo podcast hablaremos de actuación en público y de la creación de un universo original. Estamos encantadas de recibir a May que representa su grupo Badlands, un grupo que mezcla el country, el folk y el bluegrass para proponernos una experiencia única.",
-    date: "2024-03-07"
+    date: "2024-03-07",
   },
-  { title:"Entrevista a Maar",
-  url:"https://www.ivoox.com/player_ej_125275861_6_1.html?c1=2C3639",
-  description:"Hoy entrevistamos a Maar! Una cantautora valenciana que compone y produce sus propias canciones. Tiene un estilo relajado que transmite calma. Además en este capítulo tenemos la sección ¿Qué prefieres? Donde Maar se enfrentará a preguntas difíciles donde tendrá que escoger entre cantar desafinado o no tocar nunca más la guitarra! También cantará en exclusiva uno de sus últimos temas… \"el bar.\"",
-  date: "2024-03-01"},
+  {
+    title: "Entrevista a Maar",
+    url: "https://www.ivoox.com/player_ej_125275861_6_1.html?c1=" + color,
+    description:
+      'Hoy entrevistamos a Maar! Una cantautora valenciana que compone y produce sus propias canciones. Tiene un estilo relajado que transmite calma. Además en este capítulo tenemos la sección ¿Qué prefieres? Donde Maar se enfrentará a preguntas difíciles donde tendrá que escoger entre cantar desafinado o no tocar nunca más la guitarra! También cantará en exclusiva uno de sus últimos temas… "el bar."',
+    date: "2024-03-01",
+  },
   {
     title: "PM Palme",
-    url: "https://www.ivoox.com/player_ej_124394773_6_1.html?c1=2C3639",
+    url: "https://www.ivoox.com/player_ej_124394773_6_1.html?c1=" + color,
     description:
       "Hoy entrevistamos a Víctor, cantante de Peme Palme, un grupo con un toque veraniego y muy buen rollo, con canciones pegadizas y con mucho ritmo. Su trayectoria empezó en 2017 en Cullera, un lugar que les ha inspirado para componer sus canciones. ¡Escúcha sus inicios y sus consejos! Además, hoy hay una sección especial en la que tendrás que adivinar si el nombre de una banda es ficticio o de los noventa, ¿crees que existió No me pises que llevo chanclas? o ¿Písame que llevo converse?",
-    date: "2024-02-16" // Suponiendo que tengas una propiedad de fecha para cada podcast
-
+    date: "2024-02-16", // Suponiendo que tengas una propiedad de fecha para cada podcast
   },
   {
     title: "¡Bienvenida!",
-    url: "https://www.ivoox.com/player_ej_124120144_6_1.html?c1=2C3639",
+    url: "https://www.ivoox.com/player_ej_124120144_6_1.html?c1=" + color,
     description:
       "¡Hola! Somos Julia y Maya, estudiantes de periodismo y comunicación audiovisual. Tenemos muchas ganas e ilusión por realizar este podcast donde entrevistaremos a cantantes emergentes y profesionales de la música. Escúchanos para conocer nuestra historia y motivaciones para realizar Rolling The Music y ayudar a que los jóvenes cantantes puedan alcanzar su sueño. Somos estudiantes universitarias con motivación y pasión por la música.",
-      date: "2024-02-09" 
-  }
+    date: "2024-02-09",
+  },
 ];
